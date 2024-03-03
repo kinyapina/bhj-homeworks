1169d6370a6bbfd568e3aa3395f12dd4e38b901b
Katya Kinyapina <kinyapina.katya28@gmail.com>
2024-03-03 17:58:00 +0700

    2 задания из 3 выполнены

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,14 +1,27 @@
 const form = document.getElementById('form');
+const file = document.getElementById('file');
 const progress = document.getElementById('progress');
+const inputWrapper = document.querySelector('.input__wrapper');
 progress.value = 0;
 
 const requestURL = 'https://students.netoservices.ru/nestjs-backend/upload';
 
 form.addEventListener('submit', (event) => {
-  let formData = new FormData(form);
-  let request = new XMLHttpRequest();
+  event.preventDefault();
 
-  request.open('POST',)
+  const formData = new FormData();
+  formData.append('photo', file.files[0]);
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', requestURL);
+  xhr.upload.onprogress = function (event) {
+    progress.value = event.loaded / event.total;
+  }
+  xhr.send(formData);
 })
 
+/* Вопрос
+отправляю объект formData по адресу, указаному в задании
+сервер в ответ дает ошибку message:"Unexpected field" 
+Какое поле ожидает backend для отправки файла картинки?*/
 
